7f9b8c4926a20d5427578f4df97815d28f54a603
xiaohu <richardsss467@gmail.com>
2022-10-23 22:57:51 +0800

    supply some resources about list partition

--- dataStruct/List/partition.go
+++ dataStruct/List/partition.go
@@ -1,5 +1,11 @@
 package list
 
+// 力扣第 86 题
+// 单链表的分解
+// https://leetcode.cn/problems/partition-list/description/
+
+// https://labuladong.github.io/algo/1/4/#单链表的分解
+
 func Partition(header *ListNode, x int) *ListNode {
 	// 比 x 小的新链表的虚拟头节点
 	dummy1 := new(ListNode)
@@ -18,11 +24,13 @@ func Partition(header *ListNode, x int) *ListNode {
 			p2.Next = p
 			p2 = p2.Next
 		}
-		p = p.Next
-
+		// 断开原链表中每个节点的 next 指针
+		tmp := p.Next
+		p.Next = nil
+		p = tmp
 	}
-	// 一定要把较大的链表的最后一个元素的 next 指向 nil，不然可能成环
-	p2.Next = nil
+	// // 一定要把较大的链表的最后一个元素的 next 指向 nil，不然可能成环
+	// p2.Next = nil
 	p1.Next = dummy2.Next
 	return dummy1.Next
 }
